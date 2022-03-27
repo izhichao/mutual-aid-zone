842a660a4a7f4d45098ee08f2bd183a9d1c113f0
izhichao <60996631+izhichao@users.noreply.github.com>
2022-03-27 22:15:09 +0800

    使用less重构底栏；使用iconfont替代部分图片

--- src/components/Tab/FooterTab.vue
+++ src/components/Tab/FooterTab.vue
@@ -1,20 +1,20 @@
 <template>
-  <footer>
-    <router-link to="/" class="sub">
-      <img :src="url1" />
-      <p :class="indexFontStatus ? 'on' : ''">互助圈</p>
+  <div class="footer">
+    <router-link to="/" class="footer__sub">
+      <i class="iconfont" :class="{ on: indexFontStatus }">&#xe600;</i>
+      <p :class="{ on: indexFontStatus }">互助圈</p>
     </router-link>
 
-    <router-link to="/task" class="sub">
-      <img :src="url2" />
-      <p :class="$route.path === '/task' ? 'on' : ''">任务</p>
+    <router-link to="/task" class="footer__sub">
+      <i class="iconfont" :class="{on: $route.path === '/task'}">&#xe651;</i>
+      <p :class="{on: $route.path === '/task'}">任务</p>
     </router-link>
 
-    <router-link to="/store" class="sub">
-      <img :src="url3" />
-      <p :class="$route.path === '/store' ? 'on' : ''">商城</p>
+    <router-link to="/store" class="footer__sub">
+      <i class="iconfont" :class="{on: $route.path === '/store'}">&#xe688;</i>
+      <p :class="{on: $route.path === '/store'}">商城</p>
     </router-link>
-  </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -24,29 +24,6 @@ export default defineComponent({
   name: 'FooterTab',
   setup() {
     const route = useRoute();
-    // 使用计算属性动态修改底栏图标开启与关闭状态
-    const url1 = computed(() => {
-      // 通过正则表达式来匹配以/helpme开头的路径
-      if (/^\/helpme/g.test(route.path) || route.path === '/ihelp') {
-        return '/src/assets/home_on.png';
-      } else {
-        return '/src/assets/home_off.png';
-      }
-    });
-    const url2 = computed(() => {
-      if (route.path === '/task') {
-        return '/src/assets/task_on.png';
-      } else {
-        return '/src/assets/task_off.png';
-      }
-    });
-    const url3 = computed(() => {
-      if (route.path === '/store') {
-        return '/src/assets/store_on.png';
-      } else {
-        return '/src/assets/store_off.png';
-      }
-    });
     const indexFontStatus = computed(() => {
       if (/^\/helpme/g.test(route.path) || route.path === '/ihelp') {
         return true;
@@ -55,47 +32,45 @@ export default defineComponent({
       }
     });
     return {
-      url1,
-      url2,
-      url3,
       indexFontStatus
     };
   }
 });
 </script>
 
-<style scoped>
-footer {
+<style lang="less" scoped>
+.footer {
   display: flex;
-  height: 4rem;
-  width: 100%;
+  height: (64vw / 3.75);
   align-items: center;
-  position: fixed;
+  position: absolute;
+  left: 0;
+  right: 0;
   bottom: 0;
   background-color: #fff;
   box-shadow: #666 0px 0px 3px;
-}
 
-footer .sub {
-  flex: 1;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+  &__sub {
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-.sub img {
-  height: 2rem;
-  width: 2rem;
-}
+    i {
+      color: #707070;
+      font-size: (28vw / 3.75);
+    }
 
-.sub p {
-  font-size: 15px;
-  color: #707070;
-}
+    p {
+      color: #707070;
+      font-size: (16vw / 3.75);
+    }
 
-.sub .on {
-  color: #950040;
+    .on {
+      color: #950040;
+    }
+  }
 }
 </style>
