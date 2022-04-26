4c6fbec9c69a3f5e11436028f66a86c208c905ae
izhichao <60996631+izhichao@users.noreply.github.com>
2022-04-26 23:22:39 +0800

    引入VantUI；重写登录页

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import router from './router';
 import axios from 'axios';
 import { createPinia } from 'pinia';
 import { AxiosStatic } from 'axios';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -13,6 +15,7 @@ app.config.globalProperties.$http = axios;
 
 app.use(router);
 app.use(pinia);
+app.use(Vant);
 app.mount('#app');
 
 // 声明axios类型
--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,108 +1,76 @@
 <template>
   <div class="login">
     <h1>登录</h1>
-    <div class="login__phone">
-      <i class="iconfont">&#xe74c;</i>
-      <input type="text" name="username" placeholder="请输入您的用户名" v-model.lazy="username" @keyup.enter="login" />
-    </div>
-    <div class="login__password">
-      <i class="iconfont">&#xe736;</i>
-      <input type="password" name="password" placeholder="请输入您的密码" v-model.lazy="password" @keyup.enter="login" />
-    </div>
-    <input type="button" class="login__loginBtn" value="登 录" @click="login" />
+    <van-cell-group inset>
+      <van-field v-model="username" type="text" label="用户名" placeholder="请输入您的用户名" @keyup.enter="login" />
+      <van-field v-model="password" type="password" label="密码" placeholder="请输入您的密码" @keyup.enter="login" />
+    </van-cell-group>
+    <van-button type="primary" class="login__loginBtn" @click="login">登 录</van-button>
     <router-link to="register" class="login__register">立即注册</router-link>
   </div>
 </template>
 
-<script lang="ts">
-import { ComponentInternalInstance, defineComponent, getCurrentInstance, ref } from 'vue';
+<script lang="ts" setup>
+import { ComponentInternalInstance, getCurrentInstance, ref } from 'vue';
 import { useRouter } from 'vue-router';
-export default defineComponent({
-  name: 'LoginView',
-  setup() {
-    const { proxy } = getCurrentInstance() as ComponentInternalInstance;
-    const username = ref('');
-    const password = ref('');
-    const router = useRouter();
-    const login = () => {
-      proxy?.$http
-        .post('/login', {
-          username: username.value,
-          password: password.value
-        })
-        .then((res) => {
-          if (res.data.code === 20000) {
-            alert('登录成功');
-            localStorage.setItem('token', res.data.token);
-            router.push('/');
-          } else {
-            alert('登录失败');
-            username.value = '';
-            password.value = '';
-          }
-        });
-    };
-
-    return {
-      username,
-      password,
-      login
-    };
-  }
-});
+import { Toast } from 'vant';
+const { proxy } = getCurrentInstance() as ComponentInternalInstance;
+const username = ref('');
+const password = ref('');
+const router = useRouter();
+const login = () => {
+  Toast('登录测试');
+  proxy?.$http
+    .post('/login', {
+      username: username.value,
+      password: password.value
+    })
+    .then((res) => {
+      if (res.data.code === 20000) {
+        alert('登录成功');
+        localStorage.setItem('token', res.data.token);
+        router.push('/');
+      } else {
+        alert('登录失败');
+        username.value = '';
+        password.value = '';
+      }
+    });
+};
 </script>
 
 <style lang="less" scoped>
+@import '../../style/variables.less';
+
 .login {
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: #eeeff4;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  background-color: @themeBgColor;
+  text-align: center;
+  // display: flex;
+  // flex-direction: column;
+  // justify-content: center;
+  // align-items: center;
 
   h1 {
-    font-size: (32vw / 3.75);
+    margin: 1rem 0 .5rem;
+    font-size: 0.32rem;
   }
-
-  &__phone,
-  &__password {
-    margin-top: (10vw / 3.75);
-    display: flex;
-
-    i {
-      font-size: (32vw / 3.75);
-      margin-right: (10vw / 3.75);
-      color: rgb(112, 112, 112);
-    }
-
-    input {
-      font-size: (16vw / 3.75);
-      width: (160vw / 3.75);
-      height: (32vw / 3.75);
-      padding: 0 (8vw / 3.75);
-      outline: none;
-      border: 1px solid #d9d9d6;
-    }
-  }
-
   &__loginBtn {
-    font-size: (16vw / 3.75);
-    margin-top: (20vw / 3.75);
-    height: (40vw / 3.75);
-    width: (192vw / 3.75);
-    outline: none;
-    color: #fff;
-    background-color: #950040;
+    display: block;
+    font-size: 0.16rem;
+    height: 0.4rem;
+    width: 2rem;
+    margin: 0.3rem auto;
   }
 
   &__register {
-    font-size: (16vw / 3.75);
-    margin-top: (15vw / 3.75);
+    display: block;
+    font-size: 0.16rem;
+    margin-top: 0.2rem;
     color: rgb(112, 112, 112);
   }
 }
