387cb6cce4f769d1593edd5172de56b2de1177ae
izhichao <60996631+izhichao@users.noreply.github.com>
2022-05-08 17:46:22 +0800

    feat: 个人信息新增收货地址；更换mock地址

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,14 +1,14 @@
 import instance from '../utils/request';
 
 export const login = (username: string, password: string) => {
-  return instance.post('/user/login', {
+  return instance.post('/api/user/login', {
     username,
     password
   });
 };
 
 export const register = (username: string, phone: string, email: string, password: string) => {
-  return instance.post('/user/register', {
+  return instance.post('/api/user/register', {
     username,
     phone,
     email,
