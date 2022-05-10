51f1c7efc8c965a8a671801c437e10e3af20ce97
izhichao <60996631+izhichao@users.noreply.github.com>
2022-05-10 22:00:42 +0800

    feat: 新增兑换商品功能

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,13 +1,29 @@
 import instance from '../utils/request';
 
+/** 获取余额 */
 export const getBalance = () => {
   return instance.get('/api/store/balance');
 };
 
+/** 获取商品列表 */
 export const getGoods = () => {
   return instance.get('/api/store');
 };
 
+/**
+ * 余额充值
+ * @param num 充值金额
+ * @returns
+ */
 export const recharge = (num: number) => {
   return instance.post('/api/store/charge', { num });
+};
+
+/**
+ * 兑换商品
+ * @param _id 商品ID
+ * @returns
+ */
+export const exchange = (_id: string) => {
+  return instance.post('/api/store/exchange', { _id });
 };
