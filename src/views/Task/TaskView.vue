2f736a91a274f371d303b2bd0c060bdb8bcd40c1
izhichao <43055550@qq.com>
2022-05-12 15:37:18 +0800

    feat: 完成我发布的与我接受的任务列表获取

--- src/views/Task/TaskView.vue
+++ src/views/Task/TaskView.vue
@@ -6,58 +6,42 @@
 
   <div class="main-content">
     <div class="task-list">
-      <!-- 将各个任务通过props的方式传递给task组件 -->
-      <Task v-for="(item, index) in taskList" :key="item.id" :item="item"></Task>
+      <Task v-for="item in taskList" :key="item._id" :item="item"></Task>
     </div>
   </div>
   <Docker :currentIndex="1" />
 </template>
 
 <script lang="ts" setup>
-import { ComponentInternalInstance, getCurrentInstance, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 import Task, { TaskProps } from '../../components/Task.vue';
 import Docker from '../../components/Docker.vue';
-const { proxy } = getCurrentInstance() as ComponentInternalInstance;
+import { getAcceptTasks, getDispatchTasks } from '../../api/task';
 
-const taskList = ref<TaskProps[]>([]);
-const active = ref(0);
-
-proxy?.$http
-  .get('/task', {
-    params: { type: 1 },
-    headers: { token: localStorage.getItem('token') }
-  })
-  .then((res) => {
-    // 将所有任务转存到taskList中
-    taskList.value = res.data;
-  });
-
-watch(
-  () => active.value,
-  (newVal) => {
-    if (newVal === 1) {
-      proxy?.$http
-        .get('/task', {
-          params: { type: newVal + 1 },
-          headers: { token: localStorage.getItem('token') }
-        })
-        .then((res) => {
-          // 将所有任务转存到taskList中
-          taskList.value = res.data;
-        });
+const useTaskList = () => {
+  const taskList = ref<TaskProps[]>([]);
+  const active = ref(0);
+  const handleTaskList = async (active: number) => {
+    if (active === 0) {
+      const { data: res } = await getDispatchTasks();
+      taskList.value = res.data;
     } else {
-      proxy?.$http
-        .get('/task', {
-          params: { type: newVal + 1 },
-          headers: { token: localStorage.getItem('token') }
-        })
-        .then((res) => {
-          // 将所有任务转存到taskList中
-          taskList.value = res.data;
-        });
+      const { data: res } = await getAcceptTasks();
+      taskList.value = res.data;
     }
-  }
-);
+  };
+
+  watch(
+    () => active.value,
+    (newVal) => {
+      handleTaskList(newVal);
+    },
+    { immediate: true }
+  );
+  return { taskList, active };
+};
+
+const { taskList, active } = useTaskList();
 </script>
 
 <style lang="less" scoped>
