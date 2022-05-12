4d9fb71b0aabd268fa6ff6ff2fe4c695d9d74623
izhichao <43055550@qq.com>
2022-05-12 17:39:10 +0800

    refactor: 整理请求代码；移除全局挂载的axios；移除pinia

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios';
-import { createPinia } from 'pinia';
-import { AxiosStatic } from 'axios';
 import Vant from 'vant';
 import 'normalize.css';
 import 'vant/lib/index.css';
 import './style/index.less';
 
 const app = createApp(App);
-const pinia = createPinia();
-axios.defaults.baseURL = 'http://192.168.123.232:7001';
-app.config.globalProperties.$http = axios;
 
 app.use(router);
-app.use(pinia);
 app.use(Vant);
 app.mount('#app');
-
-// 声明axios类型
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $http: AxiosStatic;
-  }
-}
