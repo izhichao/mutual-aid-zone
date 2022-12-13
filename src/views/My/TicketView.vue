8cb1934c626630c2702883122f3a104596b7522d
izhichao <60996631+izhichao@users.noreply.github.com>
2022-12-13 16:15:39 +0800

    feat: 我的订单与联系客服页面添加返回按钮

---
+++ src/views/My/TicketView.vue
@@ -0,0 +1,13 @@
+<template>
+  <van-nav-bar title="联系客服" left-text="返回" left-arrow @click-left="handleBack" />
+
+</template>
+
+<script lang="ts" setup>
+const handleBack = () => history.back();
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
